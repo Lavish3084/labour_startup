34b5231dbcb462a14748e54581cbd918698dbaf9
Lavish_3084 <kambojlavish3084@gmail.com>
2026-02-09 16:14:55 +0530

    updated backend for deployment

--- lib/config.dart
+++ lib/config.dart
@@ -1,4 +1,4 @@
 class Config {
   // Using 10.0.2.2 for Android Emulator to access host localhost
-  static const String baseUrl = "https://97bd-112-196-95-21.ngrok-free.app/api";
+  static const String baseUrl = "https://labour-startup.onrender.com/api";
 }
