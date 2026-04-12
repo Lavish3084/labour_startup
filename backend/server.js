227bcc7fccf660b55e6b33a3585f4a03349c2d5c
Lavish_3084 <kambojlavish3084@gmail.com>
2026-04-12 13:00:40 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -82,7 +82,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('Labour Market Backend is running');
+    res.send('Labour Market Backending is running');
 });
 
 app.listen(PORT, () => {
