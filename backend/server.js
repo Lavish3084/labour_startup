02ae2f632ac8da5670d5947b9826eb9359346a53
Lavish_3084 <kambojlavish3084@gmail.com>
2026-05-17 02:00:28 +0530

    work page bug fixed

--- backend/server.js
+++ backend/server.js
@@ -116,7 +116,7 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
-    res.send('WILL backend runninggggg');
+    res.send('WILL backend runningggg');
 });
 
 server.listen(PORT, () => {
